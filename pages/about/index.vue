1a1d4e15128caa04c635a9ab48222def356e41c6
xingyuji <1094143240@qq.com>
2019-02-25 07:16:40 +0800

    1.工会初始化配置完成；

---
+++ pages/about/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="my-title">This is about page!</h1>
+      str1({{"<"+"br"+">"}}):
+      <div v-html="str1"></div>
+      <div>{{str1}}</div>
+      <textarea v-model="str1"></textarea>
+      <br>
+      -------------------------------------------------
+      <br>
+      str2(\n):
+      <div v-html="str2"></div>
+      <div>{{str2}}</div>
+      <textarea v-model="str2"></textarea>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      str1: "Hello <br> world",
+      str2: "Hello \n world"
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  h1
+    color mainFontColor
+  
+</style>
